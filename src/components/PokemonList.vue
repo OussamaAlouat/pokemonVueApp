4b60be921981c4da9e8410b11d0bda64f90100fb
OussamaAlouat <al317257@uji.es>
2023-04-05 16:24:34 +0200

    FEAT: add pokemon list component

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <pokemon-card v-for="poke of pokemos" :pokemon="poke" :key="poke.id"></pokemon-card>
+  </div>
+</template>
+<style>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
+import { Pokemon } from '@/models/Pokemon';
+import PokemonCard from './PokemonCard.vue';
+
+@Component({
+  components: {
+    PokemonCard,
+  },
+})
+export default class PokemonList extends Vue {
+  list: Pokemon[]= [] as Pokemon[];
+
+  poke: any;
+
+  @Action('loadData')
+  loadData!: () => void;
+
+  @State('pokemons')
+  pokemos!:Pokemon[];
+
+  mounted(): void {
+    this.loadData();
+  }
+}
+</script>;
