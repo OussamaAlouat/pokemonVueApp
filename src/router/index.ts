f85cbafe899ffcfe137348b04b56de195dce3685
OussamaAlouat <al317257@uji.es>
2023-04-05 16:25:37 +0200

    FEAT: added home route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
+import PokemonHome from '@/views/PokemonHome.vue';
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PokemonHome,
+  },
 ];
 
 const router = new VueRouter({
