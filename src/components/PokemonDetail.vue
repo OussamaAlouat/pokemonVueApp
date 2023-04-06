d3228573ea7be18ee4e6cec303ff096837897f79
OussamaAlouat <al317257@uji.es>
2023-04-06 11:09:44 +0200

    FEAT: used action and getter to get the pokemon

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    
+    Pokemon detail component
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Action, Getter } from 'vuex-class';
+
+import { Pokemon } from '@/models/Pokemon';
 
 @Component
 export default class PokemonDetail extends Vue {
+  @Prop() id!: number;
 
+  public pokemon!: Pokemon;
+
+  @Getter('getPokemonById')
+  getPokemonById!: Pokemon;
+
+  @Action('setId')
+  setId!: (id: number) => void;
+
+  mounted() {
+    this.setId(this.id);
+    this.pokemon = this.getPokemonById;
+    console.log(this.pokemon);
+  }
 }
 </script>
