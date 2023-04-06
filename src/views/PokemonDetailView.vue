8c6ce35aa095e51e25516296d9225c61204f349e
OussamaAlouat <al317257@uji.es>
2023-04-06 11:09:11 +0200

    FEAT: add componet and passed id to it

--- src/views/PokemonDetailView.vue
+++ src/views/PokemonDetailView.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    Holaaaaa
+  <div v-if="idOfPokemon">
+    <pokemon-detail :id="idOfPokemon"></pokemon-detail>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import PokemonDetail from '@/components/PokemonDetail.vue';
 
 @Component({
   components: {
+    PokemonDetail,
   },
 })
-export default class PokemonDetailView extends Vue {}
+export default class PokemonDetailView extends Vue {
+  idOfPokemon!: number;
+
+  created():void {
+    this.idOfPokemon = parseInt(this.$route.params.id);
+  }
+}
 </script>
