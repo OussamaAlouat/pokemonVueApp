b916cc3b0f72879ea5a7082bf5528c351c7f2299
OussamaAlouat <al317257@uji.es>
2023-04-12 10:15:54 +0200

    FEAT: new component to view basic information

---
+++ src/components/PokemonBasicDetail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="basic-container">
+    <img :src="image" :alt="pokemon.name">
+    <div class="text-container">
+      <span>name: {{ pokemon.name }}</span>
+      <span>heigth: {{ pokemon.height }}</span>
+      <span>weigth: {{ pokemon.weight }}</span>
+      <span>Base of experience:  {{ pokemon.base_experience }}</span>
+      <span>Number of clicks: {{ clicks }}</span>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Action, Getter, State } from 'vuex-class';
+
+import { Pokemon } from '@/models/Pokemon';
+
+@Component
+export default class PokemonBasicDetail extends Vue {
+  @State('selectedPokemon')
+  pokemon!: Pokemon;
+
+  get image():string {
+    if (this.pokemon) {
+      return this.pokemon.sprites.other?.home.front_default || '';
+    }
+
+    return '';
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: inherit;
+  max-width: 300px;
+}
+
+.basic-container {
+  display: flex;
+}
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  text-align: justify;
+}
+
+@media (max-width: 554px) {
+  .basic-container {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
