d145d7d0022c4f9219d7813df09b5a55d9ff3b31
OussamaAlouat <al317257@uji.es>
2023-04-12 08:59:47 +0200

    FEAT: created  pokemon detail

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
     Pokemon detail component
+    <div v-if="specie">
+      <div>
+        <img :src="image" :alt="pokemon.name">
+      </div>
+      <div>
+        <h2>Specie</h2>
+        <div class="specie_container">
+          <span>Base of happiness: {{ specie.base_happiness }}</span>
+          <span>Capture Rate: {{ specie.capture_rate }}</span>
+          <span>Color: {{ specie.color.name }}</span>
+          <span>Habitat: {{ specie.habitat.name }}</span>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      Loading ...
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { Action, Getter } from 'vuex-class';
+import { Action, Getter, State } from 'vuex-class';
 
 import { Pokemon } from '@/models/Pokemon';
+import { Specie } from '@/models/Specie';
 
 @Component
 export default class PokemonDetail extends Vue {
@@ -21,10 +39,44 @@ export default class PokemonDetail extends Vue {
   @Action('setId')
   setId!: (id: number) => void;
 
-  mounted() {
+  @State('specie')
+  specie!: Specie;
+
+  created() {
     this.setId(this.id);
     this.pokemon = this.getPokemonById;
-    console.log(this.pokemon);
+  }
+
+  mounted() {
+    console.log(this.specie);
+  }
+
+  get image():string {
+    if (this.pokemon) {
+      return this.pokemon.sprites.other?.home.front_default || '';
+    }
+
+    return '';
   }
 }
 </script>
+
+<style scoped>
+img {
+  width: inherit;
+  max-width: 400px;
+}
+.specie_container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+@media (max-width: 554px) {
+  .specie_container {
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+</style>
